b17c3f5f4fdc528ebdb8a75edaba3ba5ac3c6633
@czlunsu <czlunsu@163.com>
2020-11-30 10:50:38 +0800

    v1.3

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+FROM shadowsocks/shadowsocks-libev:latest 
+
+MAINTAINER Czlun <weiwenbin@czlun.com>
+
+USER root
+
+#common
+ENV SERVER_ADDR=""
+ENV SERVER_PORT=""
+
+#ss-common
+ENV PASSWORD=""
+ENV METHOD="aes-256-gcm"
+ENV TIMEOUT="300"
+ENV DNS_ADDRS="8.8.8.8,8.8.4.4"
+
+#kcptun-common
+ENV KCPTUN_ARG="--mode fast3 --nocomp --dscp 46 --sockbuf 16777217 --sndwnd 4096 --rcvwnd 4096 "
+
+#ss-server
+ENV SS_SERVER_BIND="0.0.0.0"
+ENV SS_SERVER_PORT=""
+
+#kcptun-server
+ENV KCPTUN_SERVER_BIND="0.0.0.0"
+ENV KCPTUN_SERVER_PORT="4000"
+ENV KCPTUN_TARGETSVC_ADDR="127.0.0.1"
+ENV KCPTUN_TARGETSVC_PORT=""
+
+#ss-local
+ENV SS_SERVER_ADDR=""
+ENV SS_LOCAL_BIND="0.0.0.0"
+ENV SS_LOCAL_PORT="1080"
+
+#kcptun-local
+ENV KCPTUN_SERVER_ADDR=""
+ENV KCPTUN_LOCAL_BIND="0.0.0.0"
+ENV KCPTUN_LOCAL_PORT="4000"
+
+#privoxy-local
+ENV PRIVOXY_LOCAL_PORT="2080"
+
+COPY ./client_linux_amd64 ./server_linux_amd64 ./entrypoint.sh /tmp/repo/
+
+RUN set -ex \
+ && apk add --no-cache curl privoxy 
+
+RUN set -ex \
+ && ls -l /tmp/repo \
+ && mv /tmp/repo/entrypoint.sh /tmp/repo/client_linux_amd64 /tmp/repo/server_linux_amd64 /usr/bin/ \
+ && rm -rf /tmp/repo
+
+USER nobody
+
+CMD ["sh", "/usr/bin/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,134 @@
+#!/bin/env sh
+
+server() {
+
+    export SS_SERVER_PORT=${SERVER_PORT:-8388}
+    export KCPTUN_TARGETSVC_PORT="${SS_SERVER_PORT}"
+
+    ss-server \
+        -s $SS_SERVER_BIND \
+        -p $SS_SERVER_PORT \
+        -k ${PASSWORD} \
+        -m $METHOD \
+        -t $TIMEOUT \
+        -d $DNS_ADDRS \
+        -u &
+
+    # wait for ss-server
+    until (nc -nz -w 1 $SS_SERVER_BIND $SS_SERVER_PORT) do 
+        >&2 echo "$(date +%FT%T) ss is unavailibel"
+        sleep 1
+    done
+
+    echo "ss is availibel, running kcptun-server"
+    server_linux_amd64 \
+        -l ${KCPTUN_SERVER_BIND}:${KCPTUN_SERVER_PORT} \
+        -t ${KCPTUN_TARGETSVC_ADDR}:${KCPTUN_TARGETSVC_PORT}  \
+        $KCPTUN_ARG
+}
+
+
+
+client() {
+
+    print_usage() {
+        echo "usage: $0 ss|kcptun"
+        return 1
+    }
+
+    check() {
+        if [ "$1" != "ss" -a "$1" != "kcptun" ]; then
+            print_usage
+            return 1
+        fi
+
+        if [ "${SERVER_ADDR}" = "" ]; then
+            echo "SERVER_ADDR variable is empty"
+            return 1
+        fi
+    }
+
+    run_privoxy() {
+        grep -Ev "#|^$" /etc/privoxy/config  > /etc/privoxy/config.bak
+        cat /etc/privoxy/config.bak > /etc/privoxy/config
+        rm -f /etc/privoxy/config.bak
+        sed -i '/listen-address/d' /etc/privoxy/config
+        echo "listen-address  0.0.0.0:${PRIVOXY_LOCAL_PORT}" >> /etc/privoxy/config
+        echo "forward-socks5 / 127.0.0.1:${SS_LOCAL_PORT} ." >> /etc/privoxy/config
+    }
+
+    run_kcptun() {
+        exec \
+            client_linux_amd64 \
+            -r ${KCPTUN_SERVER_ADDR}:${KCPTUN_SERVER_PORT} \
+            -l ${KCPTUN_LOCAL_BIND}:${KCPTUN_LOCAL_PORT} \
+            ${KCPTUN_ARG}
+    }
+
+    run_sslocal() {
+        exec \
+            ss-local \
+            -s ${SS_SERVER_ADDR} \
+            -p ${SS_SERVER_PORT} \
+            -b ${SS_LOCAL_BIND} \
+            -l ${SS_LOCAL_PORT} \
+            -k ${PASSWORD} \
+            -m ${METHOD}
+    }
+
+    ss() {
+    	export SS_SERVER_ADDR=${SERVER_ADDR}
+    	run_sslocal
+    }
+
+    kcptun() {
+        export KCPTUN_SERVER_ADDR=${SERVER_ADDR}
+        export SS_SERVER_ADDR=${KCPTUN_LOCAL_BIND}
+        export SS_SERVER_PORT=${KCPTUN_LOCAL_PORT}
+        run_kcptun &
+        run_sslocal
+    }
+
+    #if [ "$1" = "ss" ]; then
+    #    export SS_SERVER_ADDR=${SERVER_ADDR}
+    #fi
+    #if [ "$1" = "kcptun" ]; then
+    #    export KCPTUN_SERVER_ADDR=${SERVER_ADDR}
+    #    export SS_SERVER_ADDR=${KCPTUN_LOCAL_BIND}
+    #    export SS_SERVER_PORT=${KCPTUN_LOCAL_PORT}
+    #fi
+
+    "$@"
+}
+
+print_usage() {
+    echo "usage: $0 server|client ..."
+    return 1
+}
+
+main() {
+    if [ $# -eq 0 ]; then
+        print_usage
+        return 1
+    fi
+
+    if [ "$1" != "server" -a "$1" != "client" ]; then
+        print_usage
+        return 1
+    fi
+
+    if [ "${PASSWORD}" = "" ]; then
+        echo "PASSWORD variable is empty"
+        return 1
+    fi
+
+    # sh shell不支持type命令
+    #if [ "$(type -t $1)" != "function" ]; then
+    #    print_usage
+    #    return 1
+    #fi
+
+    "$@"
+}
+
+main "$@"
